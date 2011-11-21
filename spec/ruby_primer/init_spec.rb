23774faa45d7ce189e675bd1ed5a1c0268d58f72
Jasim A Basheer <jasim@c42.in>
2011-11-22 00:11:13 +0530

    fixed build

--- spec/ruby_primer/init_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-
-describe "Setup" do
-  it "is working" do
-  end
-end
--- spec/ruby_primer/ruby_monk_model_spec.rb
+++ spec/ruby_primer/ruby_monk_model_spec.rb
@@ -1,18 +1,17 @@
-require '../spec_helper'
+require 'spec_helper'
 
 describe RubyMonkModel do
 
   describe "create a hash from the given directory" do
 
     before(:each) do
-      FIXTURES_ROOT = File.join(File.dirname(__FILE__), '..', 'fixtures', "docs")
-      @model = RubyMonkModel.new(FIXTURES_ROOT)
+      @fixtures_root = File.join(File.dirname(__FILE__), '..', 'fixtures', "docs")
+      @model = RubyMonkModel.new(@fixtures_root)
       @chapters = @model.build_hash
     end
 
     it "reads the chapters" do
       @chapters.first["short_name"].should eq "sample_chapter"
-      pp @chapters
     end
 
     it "reads the lessons of the chapters" do
@@ -22,7 +21,7 @@ describe RubyMonkModel do
 
     it "reads the content of the lesson haml" do
       lessons = @chapters.first["lessons"]
-      lessons.first["raw_content"].should eq File.read("#{FIXTURES_ROOT}/sample_chapter/sample_lesson.haml")
+      lessons.first["raw_content"].should eq File.read("#{@fixtures_root}/sample_chapter/sample_lesson.haml")
     end
   end
 end
